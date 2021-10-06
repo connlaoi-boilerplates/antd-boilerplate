51998f08a987df8cdda6bca2a0b229441528e461
Connlaoi Smith <connlaoi@gmail.com>
2021-10-06 02:49:26 -0400

    Updates for demo of HE concept layout

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -5,7 +5,7 @@ import {
   REGISTER_LOADING,
   REGISTER_SUCCESS,
   REGISTER_FAIL,
-  LOGOUT_SUCCESS
+  LOGOUT_SUCCESS,
 } from "../constants/actionTypes";
 
 import { message } from "antd";
@@ -16,40 +16,47 @@ import { randomGreeting } from "../helpers/random";
 import Config from "../constants/config";
 
 // Check token and load user
-export const loadUser = params => (dispatch, getState) => {
+export const loadUser = (params) => (dispatch, getState) => {
   // user is loading
   dispatch({ type: LOGIN_LOADING });
 
   // fetch user
-  axios({
-    method: "post",
-    url: `${Config.API_PREFIX}/auth/login`,
-    headers: {},
-    data: {
-      email: params.email,
-      password: params.password
-    }
-  })
-    .then(res => {
-      if (res.data.success === true) {
-        dispatch({
-          type: LOGIN_SUCCESS,
-          payload: { data: res.data.data, message: res.data.message }
-        });
-        message.success(randomGreeting(`${res.data.data.firstName}`));
-      } else {
-        dispatch({ type: LOGIN_FAIL, error: res.data.message });
-        message.error(res.data.message);
-      }
-    })
-    .catch(err => {
-      dispatch({ type: LOGIN_FAIL, error: err });
-      message.error(JSON.stringify(err.message));
-    });
+  // axios({
+  //   method: "post",
+  //   url: `${Config.API_PREFIX}/auth/login`,
+  //   headers: {},
+  //   data: {
+  //     email: params.email,
+  //     password: params.password
+  //   }
+  // })
+  //   .then(res => {
+  //     if (res.data.success === true) {
+  //       dispatch({
+  //         type: LOGIN_SUCCESS,
+  //         payload: { data: res.data.data, message: res.data.message }
+  //       });
+  //       message.success(randomGreeting(`${res.data.data.firstName}`));
+  //     } else {
+  //       dispatch({ type: LOGIN_FAIL, error: res.data.message });
+  //       message.error(res.data.message);
+  //     }
+  //   })
+  //   .catch(err => {
+  //     dispatch({ type: LOGIN_FAIL, error: err });
+  //     message.error(JSON.stringify(err.message));
+  //   });
+
+  // override for testing
+  dispatch({
+    type: LOGIN_SUCCESS,
+    payload: { data: {}, message: ""},
+  });
+  message.success(randomGreeting("Joe"));
 };
 
 // Logout
-export const logoutUser = params => (dispatch, getState) => {
+export const logoutUser = (params) => (dispatch, getState) => {
   // user is logging out
   dispatch({ type: LOGOUT_SUCCESS });
 };
--- src/containers/UserLayout/index.js
+++ src/containers/UserLayout/index.js
@@ -5,12 +5,15 @@ import { withRouter } from "react-router-dom";
 import {
   Layout,
   Menu,
+  Dropdown,
   Breadcrumb,
+  Button,
   Switch,
   Icon,
   Popconfirm,
-  message
+  message,
 } from "antd";
+import { MenuOutline } from "@ant-design/icons";
 
 import { toggleTheme, toggleCollapsed, toggleLayout } from "../../actions/app";
 import { logoutUser } from "../../actions/auth";
@@ -21,22 +24,366 @@ const { Header, Content, Sider } = Layout;
 
 class UserLayout extends Component {
   render(children) {
-    const {
-      handleToggleTheme,
-      handleToggleCollapsed,
-      handleToggleLayout
-    } = this.props;
+    const { handleToggleTheme, handleToggleCollapsed, handleToggleLayout } =
+      this.props;
     const { theme, collapsed, layout } = this.props.app;
     const { firstName, lastName } = this.props.auth;
 
     let app_layout = layout ? "inline" : "vertical";
     let app_theme = theme ? "dark" : "light";
     let background_color = theme ? "#001529" : "#fff";
-    let background_color_content = theme ? "#455560" : "#455560";
+    let background_color_content = theme ? "#FFFFFF" : "#FFFFFF";
     let text_color = theme ? "#fff" : "#000";
+
+    const menu = (
+      <Menu>
+        <Menu.Item key="0">
+          <span>
+            <Icon type="setting" />
+            <span> Settings</span>
+          </span>
+        </Menu.Item>
+        <Menu.Divider />
+        <Menu.Item key="language">
+          <span>
+            {/* <Icon type="global" /> */}
+            <span>
+              <Switch
+                checkedChildren="English"
+                unCheckedChildren="French"
+                checked={true}
+              />
+            </span>
+          </span>
+        </Menu.Item>
+        <Menu.Item key="theme-color">
+          <span>
+            <span>
+              <Switch
+                checkedChildren="Dark"
+                unCheckedChildren="Light"
+                checked={false}
+              />
+            </span>
+          </span>
+        </Menu.Item>
+
+        <Menu.Divider />
+        <SubMenu
+          key="preferences"
+          title={
+            <span>
+              {/* <Icon type="setting" /> */}
+              <span> Theme</span>
+            </span>
+          }
+        >
+          <Menu.Item key="theme">
+            <span>
+              <Icon type="bg-colors" />
+              <span>
+                <Switch
+                  checkedChildren="Dark"
+                  unCheckedChildren="Light"
+                  checked={theme}
+                  onClick={handleToggleTheme}
+                />
+              </span>
+            </span>
+          </Menu.Item>
+          <Menu.Item key="layout">
+            <span>
+              <Icon type="layout" />
+              <span>
+                <Switch
+                  checkedChildren="Inline"
+                  unCheckedChildren="Vertical"
+                  checked={layout}
+                  onClick={handleToggleLayout}
+                />
+              </span>
+            </span>
+          </Menu.Item>
+        </SubMenu>
+        <Menu.Divider />
+        <Menu.Item key="logout" style={{ float: "bottom" }}>
+          <Popconfirm
+            arrowPointAtCenter
+            placement="bottomLeft"
+            title="Are you sure you want to logout?"
+            onConfirm={() => {
+              this.props.history.push("/");
+              this.props.handleLogout();
+              message.success("Logged out. Have a nice day!");
+            }}
+            okText="Yes"
+            cancelText="No"
+          >
+            <span>
+              <Icon type="logout" />
+              <span> Logout</span>
+            </span>
+          </Popconfirm>
+        </Menu.Item>
+      </Menu>
+    );
+    // The debounce function receives our function as a parameter
+    const debounce = (fn) => {
+      // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+      let frame;
+
+      // The debounce function returns a new function that can receive a variable number of arguments
+      return (...params) => {
+        // If the frame variable has been defined, clear it now, and queue for next frame
+        if (frame) {
+          cancelAnimationFrame(frame);
+        }
+
+        // Queue our function call for the next frame
+        frame = requestAnimationFrame(() => {
+          // Call our function and pass any params we received
+          fn(...params);
+        });
+      };
+    };
+
+    // Reads out the scroll position and stores it in the data attribute
+    // so we can use it in our stylesheets
+    const storeScroll = () => {
+      document.documentElement.dataset.scroll = window.scrollY;
+    };
+
+    // Listen for new scroll events, here we debounce our `storeScroll` function
+    document.addEventListener("scroll", debounce(storeScroll), {
+      passive: true,
+    });
+
+    // Update scroll position for first time
+    storeScroll();
 
     return (
       <Layout className="rootContainer">
+        {/* TOP NAV */}
+        <Header
+          style={{
+            position: "fixed",
+            marginLeft: collapsed ? "80px" : "200px",
+          }}
+        >
+          <Menu
+            className="ant-header-custom"
+            theme={app_theme}
+            mode="horizontal"
+            style={{
+              lineHeight: "64px",
+            }}
+          >
+            <Menu.Item key="home" className="ant-menu-item-no-highlight">
+              <img
+                style={{
+                  height: "33px",
+                  zIndex: 1001,
+                }}
+                src="./logo_extended.png"
+                // src="./logo_stacked.png"
+                // src="./store_icon.png"
+              />
+            </Menu.Item>
+            <Menu.Item
+              key="dropdown"
+              className="ant-menu-item-no-highlight"
+              style={{ float: "right" }}
+            >
+              <Dropdown overlay={menu}>
+                <Button
+                  className="ant-dropdown-link ant-dropdown-link-user-dropdown"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  Joe Doe <Icon type="down" />
+                </Button>
+              </Dropdown>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          {/* SIDE DRAWER */}
+          <Sider
+            collapsible={false}
+            collapsed={collapsed}
+            onCollapse={handleToggleCollapsed}
+            breakpoint={700}
+            theme={app_theme}
+            style={{
+              position: "fixed",
+              height: "100%",
+            }}
+          >
+            <Menu
+              theme={app_theme}
+              mode={app_layout}
+              defaultSelectedKeys={["dashboard"]}
+              defaultOpenKeys={["dashboard"]}
+              style={{
+                height: "100%",
+                borderRight: 0,
+                boxShadow:
+                  "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+              }}
+            >
+              {/* Collapse Toggle */}
+              <Menu.Item key="collapse" onClick={handleToggleCollapsed}>
+                <span>
+                  <Icon type="menu" />
+                  <span>{collapsed ? "Expand" : ""}</span>
+                </span>
+              </Menu.Item>
+
+              {/* Menu Options */}
+              <SubMenu
+                key="patient-profile-header"
+                title={
+                  <span>
+                    <Icon type="dashboard" />
+                    <span>Patient Profile</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="patient-profile1">
+                  <span>
+                    <Icon type="dashboard" />
+                    <span>Profile Dashboard</span>
+                  </span>
+                </Menu.Item>
+                <Menu.Item key="patient-profile2">
+                  <span>
+                    <Icon type="dashboard" />
+                    <span>Contacts</span>
+                  </span>
+                </Menu.Item>
+                <Menu.Item key="patient-profile3">
+                  <span>
+                    <Icon type="dashboard" />
+                    <span>Insurance</span>
+                  </span>
+                </Menu.Item>
+                <Menu.Item key="patient-profile4">
+                  <span>
+                    <Icon type="dashboard" />
+                    <span>Care Team</span>
+                  </span>
+                </Menu.Item>
+                <Menu.Item key="patient-profile5">
+                  <span>
+                    <Icon type="dashboard" />
+                    <span>Visuals</span>
+                  </span>
+                </Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="care-plan-header"
+                title={
+                  <span>
+                    <Icon type="line-chart" />
+                    <span>Care Plan</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="careplan-1">
+                  <span>
+                    <Icon type="line-chart" />
+                    <span>Plan Dashboard</span>
+                  </span>
+                </Menu.Item>
+                <Menu.Item key="careplan-2">
+                  <span>
+                    <Icon type="line-chart" />
+                    <span>Calendar</span>
+                  </span>
+                </Menu.Item>
+              </SubMenu>
+              <SubMenu
+                key="telemed-header"
+                title={
+                  <span>
+                    <Icon type="video-camera" />
+                    <span>Telemedicine</span>
+                  </span>
+                }
+              >
+                <Menu.Item key="telemed-1">
+                  <span>
+                    <Icon type="video-camera" />
+                    <span>Instant Call</span>
+                  </span>
+                </Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+
+          {/* CONTENT */}
+          <Layout
+            style={{
+              padding: "66px 24px 24px 24px",
+              // background: background_color_content,
+              color: text_color,
+              height: "2000px",
+              background: "#fff",
+              marginLeft: collapsed ? "80px" : "200px",
+              transition: "margin 0.2s",
+            }}
+          >
+            <Breadcrumb
+              style={{
+                margin: "16px 0",
+                color: "#000",
+              }}
+            >
+              <Breadcrumb.Item>Joe Doe</Breadcrumb.Item>
+              <Breadcrumb.Item>Care Plan</Breadcrumb.Item>
+              <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content
+              className="contentContainer"
+              style={{ background: "#7b7daf", color: text_color }}
+            >
+              {/* <div
+                style={{
+                  height: "2000px",
+                  background: "linear-gradient(#a4a6db, #797BAC)",
+                  borderRadius: "10px",
+                }}
+              /> */}
+              {/* {children} */}
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  const app = state["app"];
+  const auth = state["auth"];
+  return {
+    app,
+    auth,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  handleToggleTheme: () => dispatch(toggleTheme()),
+  handleToggleCollapsed: () => dispatch(toggleCollapsed()),
+  handleToggleLayout: () => dispatch(toggleLayout()),
+  handleLogout: () => dispatch(logoutUser()),
+});
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(UserLayout)
+);
+
+{
+  /* <Layout className="rootContainer">
         <Header style={{ background: background_color, padding: "0 80px" }}>
           <img
             style={{
@@ -176,30 +523,5 @@ class UserLayout extends Component {
             </Content>
           </Layout>
         </Layout>
-      </Layout>
-    );
-  }
+      </Layout> */
 }
-
-const mapStateToProps = state => {
-  const app = state["app"];
-  const auth = state["auth"];
-  return {
-    app,
-    auth
-  };
-};
-
-const mapDispatchToProps = dispatch => ({
-  handleToggleTheme: () => dispatch(toggleTheme()),
-  handleToggleCollapsed: () => dispatch(toggleCollapsed()),
-  handleToggleLayout: () => dispatch(toggleLayout()),
-  handleLogout: () => dispatch(logoutUser())
-});
-
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(UserLayout)
-);
